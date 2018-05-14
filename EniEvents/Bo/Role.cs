c8fb36dbc5214733bcb8037671744ed10629a98a
Hermann <hsorais@graph-et-in.com>
2018-05-14 12:20:12 +0200

    BO

---
+++ EniEvents/Bo/Role.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bo
+{
+    class Role : IIdentifiable
+    {
+        /* Attributes */
+        public int Id
+        {
+            get; set;
+        }
+        public string name
+        {
+            get; set;
+        }
+    }
+}
---
+++ EniEvents/Bo/User.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bo
+{
+    class User: IIdentifiable
+    {
+        /* Attributes */
+        public int Id
+        {
+            get; set;
+        }
+
+        public string Username
+        {
+            get; set;
+        }
+
+        public string Password
+        {
+            get; set;
+        }
+
+        public string Email
+        {
+            get; set;
+        }
+
+        public string Address
+        {
+            get; set;
+        }
+
+        public string City
+        {
+            get; set;
+        }
+
+        public string Zipcode
+        {
+            get; set;
+        }
+
+        public string Phone
+        {
+            get; set;
+        }
+
+        public virtual List<Event> Events
+        {
+            get; set;
+        }
+
+
+        public virtual List<Role> roles
+        {
+            get; set;
+        }
+
+    }
+}
