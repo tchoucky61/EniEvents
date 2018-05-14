3382644d6296862d9231fe007eaa6d2914acbaa1
TREMEL Fanny <fanny.tremel2016@campus-eni.fr>
2018-05-14 12:18:22 +0200

    BO add.

---
+++ EniEvents/Bo/Event.cs
@@ -0,0 +1,24 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bo
+{
+    public class Event : IIdentifiable
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public int Duration { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+
+        public int Lat { get; set; }
+        public int Long { get; set; }
+        public virtual Thema Thema { get; set; }
+        public virtual List<Picture> Pictures { get; set; }
+    }
+}
---
+++ EniEvents/Bo/Picture.cs
@@ -0,0 +1,18 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bo
+{
+    public class Picture : IIdentifiable
+    {
+        public int Id { get; set; }
+        public string Uri { get; set; }
+
+        public virtual Event Event { get; set; }
+
+    }
+}
