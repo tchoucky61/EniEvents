3f5ef697176f759f7e17813900c37fda76c76c1e
Hermann <hsorais@graph-et-in.com>
2018-05-14 14:36:46 +0200

    startup

---
+++ EniEvents/Bo/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bo
+{
+    public static class UserRoles
+    {
+        public static string ROLE_USER = "Utilisateur";
+        public static string ROLE_ADMIN = "Administrateur";
+    }
+}
--- EniEvents/EniEvents/Startup.cs
+++ EniEvents/EniEvents/Startup.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using Bo;
+using System.Web.Security;
+using EniEvents.Models;
 
 [assembly: OwinStartupAttribute(typeof(EniEvents.Startup))]
 namespace EniEvents
@@ -8,7 +13,41 @@ namespace EniEvents
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            ConfigureAuth(app); using (var context = new ApplicationDbContext())
+            {
+                using (var transaction = context.Database.BeginTransaction())
+                {
+                    var roleManager = new RoleManager<IdentityRole>
+                    (new RoleStore<IdentityRole>(context));
+
+                    var userManager = new UserManager<ApplicationUser>
+                        (new UserStore<ApplicationUser>(context));
+
+                    if (!roleManager.RoleExists(UserRoles.ROLE_ADMIN))
+                    {
+                        roleManager.Create(new IdentityRole(UserRoles.ROLE_ADMIN));
+                        var admin = new ApplicationUser
+                        {
+                            UserName = "hsorais@graph-et-in.com",
+                            Email = "hsorais@graph-et-in.com"
+                        };
+
+                        var result = userManager.Create(admin, "Pa$$w0rd");
+                        if (result.Succeeded)
+                        {
+                            userManager.AddToRole(admin.Id, UserRoles.ROLE_ADMIN);
+
+                            if (!roleManager.RoleExists(UserRoles.ROLE_ADMIN))
+                                roleManager.Create(new IdentityRole(UserRoles.ROLE_ADMIN));
+
+                            if (!roleManager.RoleExists(UserRoles.ROLE_USER))
+                                roleManager.Create(new IdentityRole(UserRoles.ROLE_USER));
+
+                            transaction.Commit();
+                        }
+                    }
+                }
+            }
         }
     }
 }
